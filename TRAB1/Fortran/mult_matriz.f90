494ed78c8fa5d84b3d280ac2db70e7285602fafc
Danyel Clinário <52963268+DanyelC@users.noreply.github.com>
2022-05-20 04:50:12 +0000

    loops

--- TRAB1/Fortran/mult_matriz.f90
+++ TRAB1/Fortran/mult_matriz.f90
@@ -2,7 +2,7 @@ PROGRAM Main
   use, intrinsic :: iso_fortran_env, only: dp=>real64
   implicit none
   real(dp)  :: u, i_b, f_b, i_b2, f_b2
-  integer  :: i, j, count_
+  integer  :: i, j, count_, contador
   integer, parameter  :: n = 4
   real(dp), dimension (n,n)  ::  A
   real(dp), dimension (n)  ::  x, b, b2
@@ -11,60 +11,61 @@ PROGRAM Main
   real(dp) :: intl
 
   print *, "N, GB, t1, t2"
-  
+
   DO count_ = 1,3
     CALL get_command_argument(count_, arg)
     IF (LEN_TRIM(arg) == 0) EXIT
     !WRITE (*,*) TRIM(arg)
     !write(*, '(a35)', advance = "no") arg
     read( arg, '(d10.0)' )  intl
-    matrix_lenght_r = matrix_lenght(floor(intl))
-    !write(*, '(a35)', advance = "no") floor(matrix_lenght_r)
-  ! Matriz A
-    do i = 1, n
-      do j = 1, n
-        call random_number(u)
-        A(i,j) = u
-      end do
-    end do
-  
-  
-    ! Vetor x
-    do i = 1, n
-      call random_number(u)
-      x(i) = u
-    end do
-    
-  
-  ! Metodo 1
-  
-    ! Vetor b
-    b = 0.0
-    call cpu_time(i_b)
-    do i = 1, n
-      do j = 1, n
-        b(i) = b(i) + A(i,j) * x(j)
-      end do
-    end do
-    call cpu_time(f_b)
-  
-  
-  ! Metodo 2
-  
-    ! Vetor b2
-    b2 = 0.0
-    call cpu_time(i_b2)
-    do j = 1, n
+    DO contador = 1,10
+      matrix_lenght_r = matrix_lenght(floor(intl))
+      !write(*, '(a35)', advance = "no") floor(matrix_lenght_r)
+    ! Matriz A
       do i = 1, n
-        b2(i) = b2(i) + A(i,j) * x(j)
+        do j = 1, n
+          call random_number(u)
+          A(i,j) = u
+        end do
       end do
-    end do
-    call cpu_time(f_b2)
-  
-  
-    ! ------------------------------------------------------ Results time
-    print '(I0, ",", I0, ",", f10.6, ",", f10.6)', n, floor(intl), f_b - i_b, f_b2 - i_b2
-
+    
+    
+      ! Vetor x
+      do i = 1, n
+        call random_number(u)
+        x(i) = u
+      end do
+      
+    
+    ! Metodo 1
+    
+      ! Vetor b
+      b = 0.0
+      call cpu_time(i_b)
+      do i = 1, n
+        do j = 1, n
+          b(i) = b(i) + A(i,j) * x(j)
+        end do
+      end do
+      call cpu_time(f_b)
+    
+    
+    ! Metodo 2
+    
+      ! Vetor b2
+      b2 = 0.0
+      call cpu_time(i_b2)
+      do j = 1, n
+        do i = 1, n
+          b2(i) = b2(i) + A(i,j) * x(j)
+        end do
+      end do
+      call cpu_time(f_b2)
+    
+    
+      ! ------------------------------------------------------ Results time
+      print '(I0, ",", I0, ",", f10.6, ",", f10.6)', n, floor(intl), f_b - i_b, f_b2 - i_b2
+    END DO
   END DO
 
 contains
