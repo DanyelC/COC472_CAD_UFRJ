4530516112519dbc75363eafd4441e84b35d78c4
Danyel Clinário <52963268+DanyelC@users.noreply.github.com>
2022-05-21 07:48:49 +0000

    python code

--- TRAB1/Python/graficos.py
+++ TRAB1/Python/graficos.py
@@ -1,4 +1,63 @@
 import pandas as pd
 import numpy as np
+import matplotlib as plt
 
-print("s")
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+headers = ['Name', 'Age', 'Marks']
+
+fort = pd.read_csv('Fortran\\fort.csv')
+c = pd.read_csv('C\\c.csv')
+
+c.columns = ['N', 'GB', 't1', 't2']
+fort.columns = ['N', 'GB', 't1', 't2']
+
+font_t = {'family': 'serif',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 16,
+        }
+
+font_l = {'family': 'serif',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 13,
+        }
+
+font_l2 = {'family': 'serif',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 11,
+        }
+
+x = fort['N']
+y = fort['t1']
+z = fort['t2']
+# plot
+plt.plot(x,y)
+plt.plot(x,z)
+# beautify the x-labels
+plt.gcf().autofmt_xdate()
+plt.legend(["Método linha-coluna","Método coluna-linha"], shadow=True, prop={'family':'serif','weight':'roman','size':12})
+plt.title('Tempo de execução em Fortran', fontdict=font_t)
+plt.ylabel('tempo (s)', fontdict = font_l)
+plt.xlabel('Dimensão dos arrays', fontdict = font_l)
+#plt.subplots_adjust(left=0.15)
+plt.show()
+
+
+x = c['N']
+y = c['t1']
+z = c['t2']
+# plot
+plt.plot(x,y)
+plt.plot(x,z)
+# beautify the x-labels
+plt.gcf().autofmt_xdate()
+plt.legend(["Método linha-coluna","Método coluna-linha"], shadow=True, prop={'family':'serif','weight':'roman','size':12})
+plt.title('Tempo de execução em C', fontdict=font_t)
+plt.ylabel('tempo (s)', fontdict = font_l)
+plt.xlabel('Dimensão dos arrays', fontdict = font_l)
+
+plt.show()
